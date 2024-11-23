86e7912ee9643644ddeb344c001ad88639fb473a
Jasmina <123311005+jasminades@users.noreply.github.com>
2024-11-23 11:20:14 +0100

    cbg_process

--- cbg_process/common_code.cpp
+++ cbg_process/common_code.cpp
@@ -5,12 +5,10 @@ fsiv_convert_image_byte_to_float(const cv::Mat &img)
 {
     CV_Assert(img.depth() == CV_8U);
     cv::Mat out;
-    //! TODO
-    // Hint: use cv::Mat::convertTo().
-    
+   
     img.convertTo(out, CV_32F, 1.0 / 255.0);
 
-    //
+    
     CV_Assert(out.rows == img.rows && out.cols == img.cols);
     CV_Assert(out.depth() == CV_32F);
     CV_Assert(img.channels() == out.channels());
@@ -22,12 +20,10 @@ fsiv_convert_image_float_to_byte(const cv::Mat &img)
 {
     CV_Assert(img.depth() == CV_32F);
     cv::Mat out;
-    //! TODO
-    // Hint: use cv::Mat::convertTo()
-
+    
     img.convertTo(out, CV_8U, 255.0);
 
-    //
+    
     CV_Assert(out.rows == img.rows && out.cols == img.cols);
     CV_Assert(out.depth() == CV_8U);
     CV_Assert(img.channels() == out.channels());
@@ -39,13 +35,10 @@ fsiv_convert_bgr_to_hsv(const cv::Mat &img)
 {
     CV_Assert(img.channels() == 3);
     cv::Mat out;
-    //! TODO
-    // Hint: use cvtColor.
-    // Remember: the input color scheme is assumed to be BGR.
-
+    
     cv::cvtColor(img, out, cv::COLOR_BGR2HSV);
 
-    //
+    
     CV_Assert(out.channels() == 3);
     return out;
 }
@@ -55,13 +48,10 @@ fsiv_convert_hsv_to_bgr(const cv::Mat &img)
 {
     CV_Assert(img.channels() == 3);
     cv::Mat out;
-    //! TODO
-    // Hint: use cvtColor.
-    // Remember: the ouput color scheme is assumed to be BGR.
-
+    
     cv::cvtColor(img, out, cv::COLOR_HSV2BGR);
 
-    //
+    
     CV_Assert(out.channels() == 3);
     return out;
 }
@@ -73,12 +63,7 @@ fsiv_cbg_process(const cv::Mat &in,
 {
     CV_Assert(in.depth() == CV_8U);
     cv::Mat out;
-    // TODO
-    // Hint: convert to float range [0,1] before processing the image.
-    // Hint: use cv::pow() to apply the gamma parameter.
-    // Hint: if input channels is 3 and only luma is required, convert to HSV
-    //       color space and process only de V (luma) channel.
-
+    
 
 
     cv::Mat img_float;
@@ -94,7 +79,7 @@ fsiv_cbg_process(const cv::Mat &in,
         cv::split(hsv, channels);
 
 
-        // lumma channel
+        
         cv::Mat luma;
         cv::pow(channels[2], gamma, luma);
         channels[2] = contrast * luma + brightness;
@@ -103,7 +88,7 @@ fsiv_cbg_process(const cv::Mat &in,
 
         cv::cvtColor(hsv, img_float, cv::COLOR_HSV2BGR);
     }else{
-        // gamma
+        
         cv::Mat temp;
         cv::pow(img_float, gamma, temp);
         img_float = contrast * temp + brightness;
@@ -114,7 +99,7 @@ fsiv_cbg_process(const cv::Mat &in,
     
 
 
-    //
+    
     CV_Assert(out.rows == in.rows && out.cols == in.cols);
     CV_Assert(out.depth() == CV_8U);
     CV_Assert(out.channels() == in.channels());
