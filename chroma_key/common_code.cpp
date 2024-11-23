ed89af9d65ef272fbbd269e9a31971f8ad051c5a
Jasmina <123311005+jasminades@users.noreply.github.com>
2024-11-23 11:21:09 +0100

    chroma_key

--- chroma_key/common_code.cpp
+++ chroma_key/common_code.cpp
@@ -8,14 +8,12 @@ fsiv_convert_bgr_to_hsv(const cv::Mat &img)
 {
     CV_Assert(img.channels() == 3);
     cv::Mat out;
-    //! TODO
-    // Hint: use cvtColor.
-    // Remember: the input color scheme is assumed to be BGR.
+
 
     cv::cvtColor(img, out, cv::COLOR_BGR2HSV);
 
 
-    //
+    
     CV_Assert(out.channels() == 3);
     return out;
 }
@@ -28,12 +26,11 @@ fsiv_combine_images(const cv::Mat &img1, const cv::Mat &img2,
     CV_Assert(img2.type() == img1.type());
     CV_Assert(mask.size() == img1.size());
     cv::Mat output = img1.clone(); 
-    //! TODO
-    //  HINT: you can use cv::Mat::copyTo().
+  
 
     img2.copyTo(output, mask);
 
-    //
+    
     CV_Assert(output.size() == img1.size());
     CV_Assert(output.type() == img1.type());
     return output;
@@ -47,13 +44,12 @@ fsiv_create_mask_from_hsv_range(const cv::Mat &hsv_img,
     CV_Assert(hsv_img.channels() == 3);
     cv::Mat mask;
 
-    // TODO
-    // Hint: use cv::inRange to get the mask.
+
 
 
     cv::inRange(hsv_img, lower_bound, upper_bound, mask);
 
-    //
+    
     CV_Assert(mask.size() == hsv_img.size());
     CV_Assert(mask.depth() == CV_8U);
     return mask;
@@ -64,13 +60,9 @@ fsiv_apply_chroma_key(const cv::Mat &foreg, const cv::Mat &backg, int hue,
                       int sensitivity)
 {
     cv::Mat out;
-    cv::Scalar lower_b, upper_b; // HSV range.
+    cv::Scalar lower_b, upper_b; 
 
-    // TODO
-    // Hint: use fsiv_xxx defined functions.
-    // Hint: use all ranges [0,255] for channels S and V.
-    // Remember: the backg img must have the same dimensions to combine with
-    //   foreg img. You can use cv::resize to assure this.
+
 
 
 
@@ -78,7 +70,7 @@ fsiv_apply_chroma_key(const cv::Mat &foreg, const cv::Mat &backg, int hue,
     cv::resize(backg, resized_background, foreg.size());
 
 
-    // hsv range
+   
     int upper_hue = hue + sensitivity;
     int lower_hue = hue - sensitivity;
 
@@ -86,17 +78,17 @@ fsiv_apply_chroma_key(const cv::Mat &foreg, const cv::Mat &backg, int hue,
     cv::Scalar lower_bound(lower_hue, 100, 100);
 
 
-    // mask
+
     cv::Mat hsv_foreground = fsiv_convert_bgr_to_hsv(foreg);
     cv::Mat mask = fsiv_create_mask_from_hsv_range(hsv_foreground, lower_bound, upper_bound);
 
 
-    // foreground
+ 
     cv::Mat inv_mask;
     cv::bitwise_not(mask, inv_mask);
 
 
-    // combining the images
+
     cv::Mat foreground = fsiv_combine_images(foreg, foreg, inv_mask);
     cv::Mat background = fsiv_combine_images(resized_background, resized_background, mask );
 
@@ -104,8 +96,6 @@ fsiv_apply_chroma_key(const cv::Mat &foreg, const cv::Mat &backg, int hue,
 
     
 
-
-    //
     CV_Assert(out.size() == foreg.size());
     CV_Assert(out.type() == foreg.type());
     return out;
