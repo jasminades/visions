1f1912ab5cab45fa409da2b4291c5716dcb2b637
Jasmina <123311005+jasminades@users.noreply.github.com>
2024-11-23 11:22:52 +0100

    sharpening

--- sharpening/common_code.cpp
+++ sharpening/common_code.cpp
@@ -7,13 +7,12 @@ fsiv_create_gaussian_filter(const int r)
 {
     CV_Assert(r > 0);
     cv::Mat ret_v;
-    //! TODO
-    // Hint: use cv::getGaussianKernel()
+   
 
     cv::Mat kernel = cv::getGaussianKernel(2 * r + 1, -1, CV_32F);
     ret_v = kernel * kernel.t();
 
-    //
+ 
     CV_Assert(ret_v.type() == CV_32FC1);
     CV_Assert(ret_v.rows == (2 * r + 1) && ret_v.rows == ret_v.cols);
     CV_Assert(std::abs(1.0 - cv::sum(ret_v)[0]) < 1.0e-6);
@@ -23,14 +22,11 @@ fsiv_create_gaussian_filter(const int r)
 cv::Mat fsiv_create_lap4_filter()
 {
     cv::Mat ret_v;
-    //! TODO
-    // Hint: you can use a comma-separated initializer.
-    //       See: https://docs.opencv.org/4.5.5/d6/d9e/classcv_1_1MatCommaInitializer__.html
-
+   
     ret_v = (cv::Mat_<float>(3, 3) << 0, 1, 0,
                                     1, -4, 1,
                                     0, 1, 0);
-    //
+ 
     CV_Assert(!ret_v.empty());
     CV_Assert(ret_v.rows == 3 && ret_v.cols == 3);
     CV_Assert(ret_v.type() == CV_32FC1);
@@ -40,14 +36,11 @@ cv::Mat fsiv_create_lap4_filter()
 cv::Mat fsiv_create_lap8_filter()
 {
     cv::Mat ret_v;
-    //! TODO
-    // Hint: you can use a comma-separated initializer.
-    //       See: https://docs.opencv.org/4.5.5/d6/d9e/classcv_1_1MatCommaInitializer__.html
-
+    
     ret_v = (cv::Mat_<float>(3,3) << 1, 1, 1,
                                     1, -8, 1,
                                     1, 1, 1);
-    //
+
     CV_Assert(!ret_v.empty());
     CV_Assert(ret_v.rows == 3 && ret_v.cols == 3);
     CV_Assert(ret_v.type() == CV_32FC1);
@@ -60,14 +53,11 @@ fsiv_fill_expansion(cv::Mat const &in, const int r)
     CV_Assert(!in.empty());
     CV_Assert(r > 0);
     cv::Mat ret_v;
-    //! TODO:
-    // Hint: use cv::copyMakeBorder() using the constant value 0 to fill the
-    //       expanded area.
-
+    
     cv::copyMakeBorder(in, ret_v, r, r, r, r, cv::BORDER_CONSTANT, cv::Scalar(0));
 
 
-    //
+  
     CV_Assert(ret_v.type() == in.type());
     CV_Assert(ret_v.rows == in.rows + 2 * r);
     CV_Assert(ret_v.cols == in.cols + 2 * r);
@@ -80,12 +70,10 @@ fsiv_circular_expansion(cv::Mat const &in, const int r)
     CV_Assert(!in.empty());
     CV_Assert(r > 0);
     cv::Mat ret_v;
-    //! TODO
-    //  Hint: use cv::copyMakeBorder() filling with border wrap extrapolation.
-
+    
     cv::copyMakeBorder(in, ret_v, r, r, r, r, cv::BORDER_WRAP);
 
-    //
+    
     CV_Assert(ret_v.type() == in.type());
     CV_Assert(ret_v.rows == in.rows + 2 * r);
     CV_Assert(ret_v.cols == in.cols + 2 * r);
@@ -105,18 +93,13 @@ cv::Mat fsiv_create_dog_filter(int r1, int r2)
     CV_Assert(r1 > 0 && r1 < r2);
     cv::Mat ret_v;
 
-    //! TODO
-    // Hint: use fsiv_create_gaussian_filter() and fsiv_fill_expansion() to
-    //       expand the smaller filter to be the same size as the larger one,
-    //       so you can get the difference of both.
-
+   
     cv::Mat gauss1 = fsiv_create_gaussian_filter(r1);
     cv::Mat gauss2 = fsiv_create_gaussian_filter(r2);
 
     gauss1 = fsiv_fill_expansion(gauss1, r2 - r1);
 
     ret_v = gauss2 - gauss1;
-    //
 
     CV_Assert(!ret_v.empty());
     CV_Assert(ret_v.rows == (2 * r2 + 1) && ret_v.cols == (2 * r2 + 1));
@@ -130,9 +113,7 @@ fsiv_create_sharpening_filter(const int filter_type, int r1, int r2)
     CV_Assert(0 <= filter_type && filter_type <= 2);
     CV_Assert(filter_type != 2 || (0 < r1 && r1 < r2));
     cv::Mat filter;
-    //! TODO
-    //  Remember: sharpening filter = -Laplacian + [1]
-
+    
     if(filter_type == 0){
         filter = fsiv_create_lap4_filter();
     }
@@ -148,7 +129,6 @@ fsiv_create_sharpening_filter(const int filter_type, int r1, int r2)
     filter = identity - filter;
 
 
-    //
     CV_Assert(!filter.empty() && filter.type() == CV_32FC1);
     CV_Assert((filter_type == 2) || (filter.rows == 3 && filter.cols == 3));
     CV_Assert((filter_type != 2) || (filter.rows == (2 * r2 + 1) &&
@@ -165,16 +145,7 @@ fsiv_image_sharpening(const cv::Mat &in, int filter_type,
     CV_Assert(0 <= filter_type && filter_type <= 2);
     cv::Mat out;
 
-    //! TODO
-    // Remember: The effect consists of performing a convolution of the input
-    //           image with the appropriate sharpening filter.
-    // Hint: use cv::filter2D to convolve. In this case, because the input
-    //       image is already extended, you should use 'cv::BORDER_ISOLATED'
-    //       to do nothing to the extended area.
-    // Remember: the convolved output image has the extended size but we need
-    //           to return one with size equal to the input size. You can use
-    //           cv::Mat::copyTo on a centered window to extract the result.
-
+   
     cv::Mat filter = fsiv_create_sharpening_filter(filter_type, r1, r2);
     cv::Mat expanded_in = circular ? fsiv_circular_expansion(in, r2) : fsiv_fill_expansion(in, r2);
     
@@ -184,7 +155,7 @@ fsiv_image_sharpening(const cv::Mat &in, int filter_type,
     out = convolved(cv::Rect(r2, r2, in.cols, in.rows)).clone();
 
     
-    //
+   
     CV_Assert(out.type() == in.type());
     CV_Assert(out.size() == in.size());
     return out;
