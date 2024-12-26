a4c1e7d4618a291485ba69092b1408401af0edfb
Jasmina <123311005+jasminades@users.noreply.github.com>
2024-12-26 12:21:53 +0100

    update show_BAA500.cpp

--- pollen grain classification/show_BAA500.cpp
+++ pollen grain classification/show_BAA500.cpp
@@ -1,9 +1,4 @@
-/*!
- This program shows the images of the dataset.
 
- 2024: UPTATED by Rafael Berral
-
-*/
 
 #include <iostream>
 #include <string>
@@ -20,9 +15,8 @@ const char * keys =
     "{@data          |<none>| folder with the dataset.}"
     ;
 
-// Check if this is a Windows system
 #ifdef _WIN32
-   // Arrow keys are different in Windows: ASCII codes of 'n' and 'm'
+
     static const int LEFT_ARROW = 110;
     static const int RIGHT_ARROW = 109;
 #else
@@ -57,9 +51,7 @@ main (int argc, char* const* argv)
       const int dataset_size = X.rows;
       std::cout << "Loaded " << dataset_size << " images." << std::endl;
 
-      // TODO: print the dimensionalities of X and y
-      // ...
-      
+   
       int key = 0;
       int idx = 0;
       std::string wname = "IMAGE";
@@ -68,15 +60,12 @@ main (int argc, char* const* argv)
       do
       {          
           cv::Mat img_v = X.row(idx);
-          // TODO: use reshape to convert the row into a NxN image
+         
           cv::Mat img;
-          // ...
+      
                     
           cv::imshow(wname, img);
-
-          // TODO: Print the label of the current image: fsiv_get_dataset_label_name()
-          // ...
-          // std::cout << "Idx " << idx << ": "                    
+                 
              
           key = cv::waitKey(0) & 0xff;
           
