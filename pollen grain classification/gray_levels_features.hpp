e67b9da276b0a8f43b318f1b08ad8021bcf4e2cd
Jasmina <123311005+jasminades@users.noreply.github.com>
2024-12-26 12:20:25 +0100

    update gray_levels_features.hpp

--- pollen grain classification/gray_levels_features.hpp
+++ pollen grain classification/gray_levels_features.hpp
@@ -1,7 +1,4 @@
-/**
- *  @file gray_levels_features.hpp
- *  (C) 2022- FJMC fjmadrid@uco.es
- */
+
 #pragma once
 
 #include "features.hpp"
@@ -18,10 +15,6 @@ public:
     virtual std::string get_extractor_name() const override;
     virtual cv::Mat extract_features(const cv::Mat& img) override;
 
-    //This extractor does not need override these methods:
-    //virtual void train(const cv::Mat& samples) override;
-    //virtual bool save_model(std::string const& fname) const;
-    //virtual bool load_model(std::string const& fname);
 
 };
 
